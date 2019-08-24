c70cd7815240c0d795ab6699efb9698ce7abae77
Soonho Kwon <soonhokwon@Soonhos-MacBook-Pro.local>
2019-08-24 16:15:58 -0700

    finished fitType

--- sketches/sketch_7.js
+++ sketches/sketch_7.js
@@ -2,108 +2,121 @@
 
 var x,y,w,h,s,mw,mh;
 var letters;
-
-var startX, startY;
-var margin;
-var offset;
-var aW, aH;
-var scaleVal;
+var functionalWidth,functionalHeight;
+var marginX,marginY,marginW,marginH;
+var letterW,letterH;
+var letterScaleH,letterScaleW;
+var gapW,gapH;
 
 var displayed = [[]];
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  margin = Math.min(width/10, height/10);
+  marginX = 50;
+  marginY = 50;
+  marginW = 60;
+  marginH = 60;
+  letterScaleW = 0.8;
+  letterScaleH = 0.8;
+  functionalWidth = width-(marginX+marginW);
+  functionalHeight = height-(marginY+marginH);
 
-  // startX = 20;
-  // startY = 20;
-  offset = 50;
+  // strokeVal = 10;
 
-  s = 25;
-  w = width;
-  h = height;
-  x = 0;
-  y = 0;
   updateDict();
-
-  noFill();
-  stroke(0);
-  scaleVal = Math.min(width/4, height/8)
-  strokeWeight(Math.min(width/10, height/10));
 }
 
 function draw() {
-
   //reset each frame
   background(255);
+  console.log(displayed[0].length);
 
-  //this is just an example of a single letter, even this would be paramterized.
-  // beginShape();
-  //
-  // vertex(aW, aH - (2 * scaleVal));
-  // bezierVertex(aW, aH - (2 * scaleVal), aW, aH, aW - (2 * scaleVal), aH);
-  // vertex(aW - (2 * scaleVal), aH);
-  // vertex(startX + (2 * scaleVal), aH);
-  // bezierVertex(startX + (2 * scaleVal), aH, startX, aH, startX, aH - (2 * scaleVal));
-  // vertex(startX, aH - (2 * scaleVal));
-  // vertex(startX, startY + (2 * scaleVal));
-  // bezierVertex(startX, startY + (2 * scaleVal), startX, startY, startX + (2 * scaleVal), startY);
-  // vertex(startX + (2 * scaleVal), startY);
-  // vertex(aW - (2 * scaleVal), startY);
-  // bezierVertex(aW - (2 * scaleVal), startY, aW, startY, aW, startY + (2 * scaleVal));
-  //
-  // endShape();
+  if (displayed[0].length == 0){//nothing on screen
+    let s = "Start typing to begin"
+    textSize(Math.min(width/30,height/30));
+    noStroke();
+    fill(0);
+    textAlign(CENTER, CENTER);
+    text(s, 0, 37, width, height);
+
+    // console.log('???');
+  }
+
+  //drawing settings
+  noFill();
+  stroke(0);
+
+  //strokeWeight
+  strokeVal = Math.min((w-x),(h-y))/(displayed.length * mostInList(displayed));
+  if(strokeVal > 75){strokeVal = 75;}
+  else if (strokeVal < 10){strokeVal = 10;}
+  strokeWeight(strokeVal);
 
   for (i=0;i<displayed.length;i++){ //rows
     for (j=0;j<displayed[i].length;j++){ //columns (individual letters)
       var activeLetter = displayed[i][j];
       //setting the bounding box
-      x = j * (width/displayed[i].length) + offset;
-      y = i * (height/displayed.length) + offset;
-      w = (j+1) * (width/displayed[i].length) - offset;
-      h = (i+1) * (height/displayed.length) - offset;
+      letterW = ((width-(marginX+marginW))/displayed[i].length)*letterScaleW;
+      letterH = ((height-(marginY+marginH))/displayed.length)*letterScaleH;
+      gapW = (functionalWidth-(letterW*displayed[i].length))/(displayed[i].length-1);
+      gapH = (functionalHeight-(letterH*displayed.length))/(displayed.length-1);
+
+      x = j * (letterW+gapW)+marginX;
+      y = i * (letterH+gapH)+marginY;
+      w = (j+1) * (letterW+gapW)+marginX-gapW;
+      h = (i+1) * (letterH+gapH)+marginY-gapH;
+
+      if (j==0){ //first column
+        x = marginX;
+        if (i==0){
+          y = marginY;
+        }
+        if (j==displayed[i].length-1){w = width - marginW;}
+        if (i==displayed.length-1){h = height - marginH;}
+      }
+      else if (i==0){ //first row
+        y = marginY;
+        if (i==displayed.length-1){h = height - marginH;} //if only 1 row
+        if (j==displayed[i].length-1){w = width - marginW;} //if last letter in first row
+      }
+      else if (j==displayed[i].length-1){ //last column
+        w = width - marginW;
+        if (i==displayed.length-1){h = height - marginH;} //if last element
+      }
+      else if (i==displayed.length-1){ //last row
+        h = height - marginH;
+      }
+
       mw = (x+w)/2;
       mh = (y+h)/2;
       s = Math.min((w-x)/4, (h-y)/8);
-      strokeWeight(Math.min((w-x)/15, (h-y)/15));
-
-      //haven't figured out offset yet.
-      // if (offset > 0){offset = 50-(2 * displayed[i].length);}
-      // offset = 50/(displayed[i].length);
+      // strokeWeight(Math.min((w-x), (h-y)) / (displayed.length * displayed[i].length));
 
       updateDict();
-
-      for (k=0;k<letters[activeLetter].length;k++){ //points in each letter
-
-        // fill(random(0,255),random(0,255),random(0,255));
-        // rect(x,y,w,h);
-
-        var vertexList = letters[activeLetter][k];
-        // console.log(vertexList[0]);
-
-        beginShape();
-        for (l=0;l<vertexList.length;l++){
-          if (vertexList[l][0] == 0){
-            vertex(vertexList[l][1][0],vertexList[l][1][1]);
+      if (activeLetter in letters){
+        for (k=0;k<letters[activeLetter].length;k++){ //points in each letter
+          var vertexList = letters[activeLetter][k];
+          beginShape();
+          for (l=0;l<vertexList.length;l++){
+            if (vertexList[l][0] == 0){
+              vertex(vertexList[l][1][0],vertexList[l][1][1]);
+            }
+            else if (vertexList[l][0] == 1){
+              bezierVertex(vertexList[l][1][0],vertexList[l][1][1],vertexList[l][2][0],vertexList[l][2][1],vertexList[l][3][0],vertexList[l][3][1]);
+            }
           }
-          else if (vertexList[l][0] == 1){
-            bezierVertex(vertexList[l][1][0],vertexList[l][1][1],vertexList[l][2][0],vertexList[l][2][1],vertexList[l][3][0],vertexList[l][3][1]);
-          }
+          endShape();
         }
-        endShape();
       }
     }
   }
-
-
 }
 
 function keyPressed() {
   if (keyCode > 64 && keyCode < 91){
     var letter = key.toUpperCase();
     if (letter in letters){displayed[displayed.length-1].push(letter);}
-    console.log(displayed);
   }
 }
 
@@ -115,22 +128,16 @@ function keyReleased(){
     else if (displayed[displayed.length-1].length == 0 && displayed.length > 1){
       displayed.pop();
     }
-    console.log(displayed);
   }
-
-
-  else if (keyCode == 13){
-    displayed.push([]);
+  else if (keyCode == 32  ){
+    displayed[displayed.length-1].push(" ");
   }
-}
-
-function newLetter(letter) {
-
+  else if (keyCode == 13){displayed.push([]);}
+  else if (keyCode == 49){save('myCanvas.png')}
 }
 
 function updateDict() {
   letters =
-
   {
     "A":[[[0,[x,h]],[0,[x,y+2*s]],[1,[x,y+2*s],[x,y],[x+2*s,y]],[0,[x+2*s,y]],[0,[w-2*s,y]],[1,[w-2*s,y],[w,y],[w,y+2*s]],[0,[w,y+2*s]],[0,[w,h]]],[[0,[x,mh]],[0,[w,mh]]]],
     "B":[[[0,[x,mh]],[0,[x,y]],[0,[w-2*s,y]],[1,[w-2*s,y],[w,y],[w,y+2*s]],[0,[w,y+2*s]],[0,[w,mh-2*s]],[1,[w,mh-2*s],[w,mh],[w-2*s,mh]],[0,[w-2*s,mh]],[0,[x,mh]],[0,[x,h]],[0,[w-2*s,h]],[1,[w-2*s,h],[w,h],[w,h-2*s]],[0,[w,mh+2*s]],[1,[w,mh+2*s],[w,mh],[w-2*s,mh]],[w-2*s,mh]]],
@@ -163,17 +170,25 @@ function updateDict() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  // aW = width - 55;
-  // aH = height - 55;
-  w = width;
-  h = height;
-
-  startX = margin;
-  startY = margin
-  aW = width - margin * 2;
-  aH = height - margin * 2;
-  scaleVal = Math.min(aW/4, aH/8);
-  var newWeight = Math.min(aW/10, aH/10);
-  strokeWeight(Math.min(aW/10, aH/10));
-  margin = Math.min(width/10, height/10);
+  functionalWidth = width-(marginX+marginW);
+  functionalHeight = height-(marginY+marginH);
 }
+
+function mostInList(list){
+  var highest = 0;
+  for (i=0;i<list.length;i++){
+    if (list[i].length > highest) {highest=list[i].length;}
+  }
+  return highest;
+}
+
+function map(value,x1,y1,x2,y2){
+  return (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+}
+//
+// function hideHelp(){
+//   var frame = 0;
+//   var finalFrame = 1000;
+//   while (frame < finalFrame){
+//   }
+// }
