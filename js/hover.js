5e55fd3e08983a47b54e5854b3576191d2f7e7af
Soonho Kwon <soonhokwon@Soonhos-MacBook-Pro-2.local>
2021-05-16 14:23:51 -0700

    hover photos

---
+++ js/hover.js
@@ -0,0 +1,15 @@
+function mouseOverSkate() {
+  document.getElementById("aboutPhoto").src="assets/skateboarding.gif"
+}
+
+function mouseOverClimb() {
+  document.getElementById("aboutPhoto").src="assets/climbing.jpg"
+}
+
+function mouseOverFieldNotes() {
+  document.getElementById("aboutPhoto").src="assets/fieldNotes.png"
+}
+
+function mouseOut() {
+  document.getElementById("aboutPhoto").src="assets/portrait.jpg"
+}
