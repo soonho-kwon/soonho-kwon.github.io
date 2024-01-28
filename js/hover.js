20d507f8e13f333c2e1d7d35d729548615cacc84
Soonho Kwon <skwon@Soonhos-MacBook-Pro.local>
2024-01-27 22:05:15 -0800

    hover preload

--- js/hover.js
+++ js/hover.js
@@ -1,15 +1,15 @@
 function mouseOverSkate() {
-  document.getElementById("aboutPhoto").src="assets/sb-2.gif"
+  document.getElementById("aboutPhoto").src="assets/sb-2.gif";
 }
 
 function mouseOverClimb() {
-  document.getElementById("aboutPhoto").src="assets/climbing.jpg"
+  document.getElementById("aboutPhoto").src="assets/climbing.jpg";
 }
 
 function mouseOverFieldNotes() {
-  document.getElementById("aboutPhoto").src="assets/fieldNotes.png"
+  document.getElementById("aboutPhoto").src="assets/fieldNotes.png";
 }
 
 function mouseOut() {
-  document.getElementById("aboutPhoto").src="assets/Portrait.jpg"
+  document.getElementById("aboutPhoto").src="assets/Portrait.jpg";
 }
